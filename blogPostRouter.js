6a0c834bd23056467b403895602e69e9b201a177
Brandon Otto <branaotto@gmail.com>
2018-04-16 23:54:45 -0400

    POST route complete

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -12,17 +12,30 @@ BlogPosts.create('Chicken Raising', 'content', 'Fred');
 BlogPosts.create('Pastured Pork', 'content', 'Barney');
 BlogPosts.create('Collecting Eggs', 'content', 'BamBam');
 
-//when root is called using GET, return all
-//current blog posts
+//GET requests will return all current blog posts
 router.get('/', (req, res) => {
     res.json(BlogPosts.get());
 });
 
-
-
+//POST requests will add a new item if required items are included
+//and return a status code 201
+//if not, log an error and return a 400 status code.
 router.post('/', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
 
+    for( let i = 0; i < requiredFields.length; i++)
+    {
+        const field = requiredFields[i];
+        if(!(field in req.body))
+        {
+            const message = `Missing ${field} in request body.`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
 
+    const post = BlogPosts.create(req.body.title, req.body.content, req.body.author)
+    res.status(201).json(post);
 });
 
 
